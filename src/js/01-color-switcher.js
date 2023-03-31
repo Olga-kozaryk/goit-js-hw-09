945889f25c74b8f462603fe707babdc516283edb
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-03-31 22:34:21 +0300

    hw-9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
-
+function getRandomHexColor() {
+    return '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0);
+  }
+  
+  function disableButton(button) {
+    button.disabled = true;
+  }
+  
+  function enableButton(button) {
+    button.disabled = false;
+  }
+  
+  const startBtn = document.querySelector('button[data-start]');
+  const stopBtn = document.querySelector('button[data-stop]');
+  
+  let intervalId;
+  
+  function startButtonHandler() {
+    disableButton(startBtn);
+    enableButton(stopBtn);
+    intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  
+  function stopButtonHandler() {
+    enableButton(startBtn);
+    disableButton(stopBtn);
+    clearInterval(intervalId);
+  }
+  
+  startBtn.addEventListener('click', startButtonHandler);
+  stopBtn.addEventListener('click', stopButtonHandler);
+  
+  disableButton(stopBtn);
